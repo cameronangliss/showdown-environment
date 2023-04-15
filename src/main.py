3711980a59d750325e8a0b34ffe82be250126676
Cameron Angliss <cameronangliss99@gmail.com>
2023-04-15 13:43:37 -0400

    small tweaks

--- src/main.py
+++ src/main.py
@@ -8,22 +8,20 @@ from player import Player
 
 
 async def main():
+    with open("config.json") as f:
+        config = json.load(f)
     logging.basicConfig(
         level=logging.INFO, filename="debug.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s"
     )
     logger = logging.getLogger()
-    with open("config.json") as f:
-        config = json.load(f)
     player1 = Player(config["username1"], config["password1"], logger)
     player2 = Player(config["username2"], config["password2"], logger)
     battle = Battle(player1, player2, logger)
     await battle.setup()
-    counter = 1
-    while True:
+    for i in range(1000):
         winner = await battle.run_episode()
         time = datetime.now().strftime("%H:%M:%S")
-        print(f"{time}: {winner} wins game {counter}")
-        counter += 1
+        print(f"{time}: {winner} wins game {i + 1}")
 
 
 if __name__ == "__main__":
