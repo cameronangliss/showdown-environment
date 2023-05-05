9308853e1a4891687c70fa40ea690492b9bbd69e
Cameron Angliss <cameronangliss99@gmail.com>
2023-05-05 12:23:33 -0400

    updating json files in main

--- src/main.py
+++ src/main.py
@@ -2,12 +2,28 @@ import asyncio
 import json
 import logging
 import os
+import requests
+import re
 import torch
 
 from trainer import Trainer
 from model import Model
 from env import Env
 from player import Player
+
+
+def update_json_file(filename: str):
+    response = requests.get(f"https://play.pokemonshowdown.com/data/{filename}.js")
+    js_text = response.text
+    i = js_text.index("{")
+    js_literal = js_text[i:-1]
+    json_text = re.sub(
+        r'(?<![\w\d"])([a-zA-Z0-9_]+)(:)',
+        r'"\1"\2',
+        js_literal,
+    )
+    with open(f"json/{filename}.json", "w") as file:
+        file.write(json_text)
 
 
 async def main():
@@ -37,6 +53,13 @@ async def main():
     file_name = f"{model_arch}_{epsilon}_{gamma}_{alpha}_{num_episodes}"
     if os.path.exists(f"saves/{file_name}.pth"):
         model.load_state_dict(torch.load(f"saves/{file_name}.pth"))
+
+    # update json files
+    update_json_file("pokedex")
+    update_json_file("moves")
+    update_json_file("abilities")
+    update_json_file("items")
+    update_json_file("typechart")
 
     # construct and run trainer
     trainer = Trainer(model, env)
