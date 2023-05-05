ac89b3925cfe6c482563a69f6c2e9d3e4df2e84a
Cameron Angliss <cameronangliss99@gmail.com>
2023-05-05 12:19:28 -0400

    models can be saved

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
 import logging
+import os
+import torch
 
 from trainer import Trainer
 from model import Model
@@ -9,8 +11,11 @@ from player import Player
 
 
 async def main():
+    # load configuration
     with open("config.json") as f:
         config = json.load(f)
+
+    # construct training environment
     logging.basicConfig(
         level=logging.INFO,
         filename="debug.log",
@@ -20,10 +25,25 @@ async def main():
     logger = logging.getLogger()
     player1 = Player(config["username1"], config["password1"], logger)
     player2 = Player(config["username2"], config["password2"], logger)
-    model = Model(10, 20, 10)
     env = Env(player1, player2, logger)
+
+    # construct model, load save file if one exists
+    model_arch = json.loads(config["model_arch"])
+    model = Model(*model_arch)
+    epsilon = float(config["epsilon"])
+    gamma = float(config["gamma"])
+    alpha = float(config["alpha"])
+    num_episodes = int(config["num_episodes"])
+    file_name = f"{model_arch}_{epsilon}_{gamma}_{alpha}_{num_episodes}"
+    if os.path.exists(f"saves/{file_name}.pth"):
+        model.load_state_dict(torch.load(f"saves/{file_name}.pth"))
+
+    # construct and run trainer
     trainer = Trainer(model, env)
-    await trainer.train(num_episodes=100, epsilon=0.1, gamma=0.9, alpha=0.01)
+    await trainer.train(num_episodes, epsilon, gamma, alpha)
+
+    # save progress
+    torch.save(trainer.model.state_dict(), f"saves/{file_name}.pth")
 
 
 if __name__ == "__main__":
