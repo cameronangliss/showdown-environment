66ecfa2f465f689a69b4b8799be4d9c5fa6bcf06
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-08 18:47:32 -0400

    fixing json finding method

--- src/pokemon_showdown_env/data/dex.py
+++ src/pokemon_showdown_env/data/dex.py
@@ -9,8 +9,8 @@ abilitydex: dict[str, dict[str, Any]] = {}
 itemdex: dict[str, dict[str, Any]] = {}
 
 for i in range(1, 10):
-    pokedex[f"gen{i}"] = json.loads(files("state").joinpath(f"data/gen{i}", "pokedex.json").read_text())
-    movedex[f"gen{i}"] = json.loads(files("state").joinpath(f"data/gen{i}", "movedex.json").read_text())
-    typedex[f"gen{i}"] = json.loads(files("state").joinpath(f"data/gen{i}", "typedex.json").read_text())
-    abilitydex[f"gen{i}"] = json.loads(files("state").joinpath(f"data/gen{i}", "abilitydex.json").read_text())
-    itemdex[f"gen{i}"] = json.loads(files("state").joinpath(f"data/gen{i}", "itemdex.json").read_text())
+    pokedex[f"gen{i}"] = json.loads(files("src/pokemon_showdown_env").joinpath(f"data/json/gen{i}", "pokedex.json").read_text())
+    movedex[f"gen{i}"] = json.loads(files("src/pokemon_showdown_env").joinpath(f"data/json/gen{i}", "movedex.json").read_text())
+    typedex[f"gen{i}"] = json.loads(files("src/pokemon_showdown_env").joinpath(f"data/json/gen{i}", "typedex.json").read_text())
+    abilitydex[f"gen{i}"] = json.loads(files("src/pokemon_showdown_env").joinpath(f"data/json/gen{i}", "abilitydex.json").read_text())
+    itemdex[f"gen{i}"] = json.loads(files("src/pokemon_showdown_env").joinpath(f"data/json/gen{i}", "itemdex.json").read_text())
