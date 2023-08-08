e723b6939f72a8892974c65b13e7632862d0a847
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-08 18:31:46 -0400

    organizing

--- scripts/dex.js
+++ scripts/dex.js
@@ -1,12 +1,14 @@
 const { Dex } = require('pokemon-showdown');
 const fs = require('fs');
 
+relPathToJsonFolder = '../src/pokemon_showdown_env/data/json'
+
 function transformData(jsonList) {
     result = {};
     for (let i = 0; i < jsonList.length; i++) {
         jsonObj = JSON.parse(JSON.stringify(jsonList[i]));
-        if (jsonObj.id == "hiddenpower" && jsonObj.name != "Hidden Power") {
-            splitName = jsonObj.name.split(" ");
+        if (jsonObj.id == 'hiddenpower' && jsonObj.name != 'Hidden Power') {
+            splitName = jsonObj.name.split(' ');
             jsonObj.id += splitName[splitName.length - 1].toLowerCase();
         }
         result[jsonObj.id] = jsonObj;
@@ -14,13 +16,17 @@ function transformData(jsonList) {
     return JSON.stringify(result, null, 4);
 }
 
+if (!fs.existsSync(relPathToJsonFolder)) {
+    fs.mkdirSync(relPathToJsonFolder);
+}
+
 for (let i = 1; i <= 9; i++) {
-    if (!fs.existsSync(`gen${i}`)) {
-        fs.mkdirSync(`gen${i}`);
+    if (!fs.existsSync(`${relPathToJsonFolder}/gen${i}`)) {
+        fs.mkdirSync(`${relPathToJsonFolder}/gen${i}`);
     }
-    fs.writeFileSync(`gen${i}/pokedex.json`, transformData(Dex.mod(`gen${i}`).species.all()));
-    fs.writeFileSync(`gen${i}/movedex.json`, transformData(Dex.mod(`gen${i}`).moves.all()));
-    fs.writeFileSync(`gen${i}/typedex.json`, transformData(Dex.mod(`gen${i}`).types.all()));
-    fs.writeFileSync(`gen${i}/abilitydex.json`, transformData(Dex.mod(`gen${i}`).abilities.all()));
-    fs.writeFileSync(`gen${i}/itemdex.json`, transformData(Dex.mod(`gen${i}`).items.all()));
+    fs.writeFileSync(`${relPathToJsonFolder}/gen${i}/pokedex.json`, transformData(Dex.mod(`gen${i}`).species.all()));
+    fs.writeFileSync(`${relPathToJsonFolder}/gen${i}/movedex.json`, transformData(Dex.mod(`gen${i}`).moves.all()));
+    fs.writeFileSync(`${relPathToJsonFolder}/gen${i}/typedex.json`, transformData(Dex.mod(`gen${i}`).types.all()));
+    fs.writeFileSync(`${relPathToJsonFolder}/gen${i}/abilitydex.json`, transformData(Dex.mod(`gen${i}`).abilities.all()));
+    fs.writeFileSync(`${relPathToJsonFolder}/gen${i}/itemdex.json`, transformData(Dex.mod(`gen${i}`).items.all()));
 }
