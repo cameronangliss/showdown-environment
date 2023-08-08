668199adc7325b1b7ce750dab6a79b2878017a13
Cameron Angliss <cameronangliss99@gmail.com>
2023-08-08 18:32:01 -0400

    minor improvement

--- scripts/dex.js
+++ scripts/dex.js
@@ -1,7 +1,7 @@
 const { Dex } = require('pokemon-showdown');
 const fs = require('fs');
 
-relPathToJsonFolder = '../src/pokemon_showdown_env/data/json'
+relPathToJsonFolder = 'src/pokemon_showdown_env/data/json'
 
 function transformData(jsonList) {
     result = {};
