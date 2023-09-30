12509d5f0cd40f74d1d8174f1a440aa144b84267
Cameron Angliss <cameronangliss99@gmail.com>
2023-09-29 21:39:31 -0500

    greatly improving efficiency

--- reinforcement_learning/model.py
+++ reinforcement_learning/model.py
@@ -61,35 +61,27 @@ class Model(nn.Module):
 
     async def improve(self):
         duplicate_model = deepcopy(self)
-        num_wins = 0
-        while num_wins < 55:
-            num_wins = await self.attempt_improve(duplicate_model)
-
-    async def attempt_improve(self, duplicate_model: Model) -> float:
-        # gathering data
-        print("Gathering experiences...")
-        new_experiences, _ = await self.__run_episodes(duplicate_model, 100)
-        self.memory.extend(new_experiences)
+        print("Generating experiences...")
+        experiences, _ = await self.__run_episodes(duplicate_model, 100)
+        self.memory.extend(experiences)
         print("Done!")
-        # training
-        print(f"Training on {len(self.memory)} experiences...")
-        for i in range(1000):
-            batch = self.memory.sample(round(len(self.memory) / 100))
-            for exp in batch:
-                self.__update(exp)
-            print(f"Progress: {(i + 1) / 10}%", end="\r")
-        print("Done!                         ")
-        # evaluating
-        print("Evaluating improved model...")
-        _, num_wins = await self.__run_episodes(duplicate_model, 100, min_win_rate=0.55)
-        print("Done!")
-        # printing result
-        if num_wins < 55:
-            print("Improvement failed.")
-        else:
-            print("Improvement succeeded!")
-            self.memory.clear()
-        return num_wins
+        while True:
+            print(f"Training on {len(self.memory)} experiences...")
+            for i in range(1000):
+                batch = self.memory.sample(round(len(self.memory) / 100))
+                for exp in batch:
+                    self.__update(exp)
+                print(f"Progress: {(i + 1) / 10}%", end="\r")
+            print("Done!                         ")
+            print("Evaluating model...")
+            experiences, num_wins = await self.__run_episodes(duplicate_model, 100, 0.55)
+            if num_wins < 55:
+                print("Improvement failed.")
+                self.memory.extend(experiences)
+            else:
+                print("Improvement succeeded!")
+                self.memory.clear()
+                break
 
     async def __run_episodes(
         self, alt_model: Model, num_episodes: int, min_win_rate: float | None = None
@@ -101,14 +93,13 @@ class Model(nn.Module):
         num_iters = 0
         time = datetime.now().strftime("%H:%M:%S")
         for i in range(num_episodes):
-            new_experiences, winner = await self.__run_episode(alt_model, env, "gen4randombattle", min_win_rate is None)
+            new_experiences, winner = await self.__run_episode(alt_model, env, "gen4randombattle")
             if len(experiences) + len(new_experiences) > self.__memory_length:
-                print("Maximum memory reached! Gathering finished early.")
                 break
             experiences += new_experiences
             if winner is None:
                 num_wins += 0.5
-            if winner == env.agent.username:
+            elif winner == env.agent.username:
                 num_wins += 1
             time = datetime.now().strftime("%H:%M:%S")
             print(f"{time}: Win rate = {num_wins}/{i + 1}", end="\r")
@@ -119,11 +110,10 @@ class Model(nn.Module):
                 break
         print(f"{time}: Win rate = {num_wins}/{num_iters}")
         await env.close()
-        meaningful_experiences = list(filter(lambda experience: experience.action is not None, experiences))
-        return meaningful_experiences, num_wins
+        return experiences, num_wins
 
     async def __run_episode(
-        self, alt_model: Model, env: Environment, format_str: str, exploring: bool
+        self, alt_model: Model, env: Environment, format_str: str
     ) -> tuple[list[Experience], str | None]:
         experiences: list[Experience] = []
         try:
@@ -132,30 +122,29 @@ class Model(nn.Module):
             done = False
             while not done:
                 turn += 1
-                action1 = self.__get_action(state1, exploring)
-                action2 = alt_model.__get_action(state2, exploring)
+                action1 = self.__get_action(state1)
+                action2 = alt_model.__get_action(state2)
                 next_state1, next_state2, reward1, reward2, done = await env.step(
                     state1,
                     state2,
                     action1,
                     action2,
                 )
-                if exploring:
-                    experience1 = Experience(
-                        encode_battle(state1).to(self.device),
-                        action1,
-                        encode_battle(next_state1).to(self.device),
-                        reward1,
-                        done,
-                    )
-                    experience2 = Experience(
-                        encode_battle(state2).to(self.device),
-                        action2,
-                        encode_battle(next_state2).to(self.device),
-                        reward2,
-                        done,
-                    )
-                    experiences += [experience1, experience2]
+                experience1 = Experience(
+                    encode_battle(state1).to(self.device),
+                    action1,
+                    encode_battle(next_state1).to(self.device),
+                    reward1,
+                    done,
+                )
+                experience2 = Experience(
+                    encode_battle(state2).to(self.device),
+                    action2,
+                    encode_battle(next_state2).to(self.device),
+                    reward2,
+                    done,
+                )
+                experiences += [experience1, experience2]
                 state1, state2 = next_state1, next_state2
             try:
                 winner_id = state1.protocol.index("win")
@@ -163,17 +152,18 @@ class Model(nn.Module):
                 winner = None
             else:
                 winner = state1.protocol[winner_id + 1].strip()
-            return experiences, winner
+            meaningful_experiences = list(filter(lambda experience: experience.action is not None, experiences))
+            return meaningful_experiences, winner
         except (ConnectionClosedError, ConnectionClosedOK):
             env.logger.error("Connection closed unexpectedly")
             await env.setup()
             winner = None
             return [], winner
 
-    def __get_action(self, state: Battle, exploring: bool) -> int | None:
+    def __get_action(self, state: Battle) -> int | None:
         action_space = state.get_valid_action_ids()
         if action_space:
-            if exploring and random.random() < self.__epsilon:
+            if random.random() < self.__epsilon:
                 action = random.choice(action_space)
             else:
                 features = encode_battle(state).to(self.device)
