dc2ab4625899f427a6e498ab6eaf561f0277d59c
Cameron Angliss <cameronangliss99@gmail.com>
2024-05-19 23:49:09 -0500

    fix

--- src/showdown_environment/data/dex.py
+++ src/showdown_environment/data/dex.py
@@ -3,7 +3,7 @@ import json
 json_path = "src/showdown_environment/data/json"
 
 pokedex = json.load(open(f"{json_path}/pokedex.json"))
-movedex = json.load(open(f"{json_path}/movedex.json"))
-typedex = json.load(open(f"{json_path}/typedex.json"))
-abilitydex = json.load(open(f"{json_path}/abilitydex.json"))
-itemdex = json.load(open(f"{json_path}/itemdex.json"))
+movedex = json.load(open(f"{json_path}/moves.json"))
+typedex = json.load(open(f"{json_path}/typechart.json"))
+abilitydex = json.load(open(f"{json_path}/abilities.json"))
+itemdex = json.load(open(f"{json_path}/items.json"))
